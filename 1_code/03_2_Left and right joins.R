fc49a6aaf6f24e7d6c9cb8c4d2b577811e255ef2
ltrangng <tranglek1@hotmail.com>
2020-09-09 21:56:20 +0200

    right join

--- 1_code/03_2_Left and right joins.R
+++ 1_code/03_2_Left and right joins.R
@@ -67,3 +67,87 @@ inventory_version_1 <- inventories %>%
 sets %>% left_join(inventory_version_1, by = "set_num") %>%
   # filter where version is na
   filter(is.na(version))
+
+# The right_join verb ####
+
+# The right joins are mirror images of left joins. A right join keeps all the observations in the second (or "right") table, whether or not they appear in the first table.
+batmobile %>%
+  right_join(batwing, by = c("part_num", "color_id"),
+             suffix = c("_batmobile", "_batwing"))
+
+# Count and sort ####
+
+# Calculate the number of sets that has each theme id
+sets %>%
+  count(theme_id, sort = TRUE)
+# The useful information for "theme_id" is in another table called "themes". Import and examine this table
+themes <- readRDS("themes.rds")
+# Add theme name for more infos
+sets %>%
+  count(theme_id, sort = TRUE) %>%
+  inner_join(themes, by = c("theme_id" = "id"))
+# Specifically, any themes that never occurred in any set in this database would not appear. A right join would keep those themes that never occurred.
+sets %>%
+  count(theme_id, sort = TRUE) %>%
+  right_join(themes, by = c("theme_id" = "id"))
+# Replace NA with zero
+sets %>%
+  count(theme_id, sort = TRUE) %>%
+  right_join(themes, by = c("theme_id" = "id")) %>%
+  replace_na(list(n = 0))
+
+# Counting part colors ####
+
+# Import the tables "parts" and "part_categories"
+parts <- readRDS("parts.rds")
+part_categories <- readRDS("part_categories.rds")
+# Sometimes you'll want to do some processing before you do a join, and prioritize keeping the second (right) table's rows instead
+parts %>%
+  count(part_cat_id, sort = TRUE) %>%
+  # count before right join because we don't only want to knowt the count of part_cat_id in "parts" but we also want to know if there is any part_cat_id not presents in "parts"
+  right_join(part_categories, by = c("part_cat_id" = "id")) %>%
+  # filter for where the column n is NA
+  filter(is.na(n == TRUE ))
+# Replace NA with zero
+parts %>%
+  count(part_cat_id, sort = TRUE) %>%
+  right_join(part_categories, by = c("part_cat_id" = "id")) %>%
+  replace_na(list(n = 0))
+
+# Joining tables to themeselves ####
+
+# In the themes table, beside theme_id and name, there's also a parent_id column. This indicates a hierarchiral table. By joining a table to itself we can explore the relationship between themes and parents
+themes %>%
+  inner_join(themes, by = c("parent_id" = "id"),
+             suffix = c("_child", "_parent"))
+# E.g, there're LEGO sets that are themed around the fantasy series "The Lords of the Rings"
+themes %>%
+  inner_join(themes, by = c("parent_id" = "id"),
+             suffix = c("_child", "_parent")) %>%
+  filter(name_child == "The Lord of the Rings")
+# Find out all of the children of that theme instead
+themes %>%
+  inner_join(themes, by = c("parent_id" = "id"),
+             suffix = c("_child", "_parent")) %>%
+  filter(name_parent == "The Lord of the Rings")
+# Based on all this, we start understanding the shape of the data.
+
+# Discover what children of the "Harry Potter" theme are
+themes %>%
+  inner_join(themes, by = c("parent_id" = "id"),
+             suffix = c("_child", "_parent")) %>%
+  filter(name_parent == "Harry Potter")
+
+# Some themes might not have any children at all, which means they won't be included in the inner join. Left join a table to itself
+themes %>%
+  left_join(themes, by = c("id" = "parent_id"), suffix = c("_parent", "_child")) %>%
+  filter(is.na(id_child) == TRUE)
+
+# Joining themes to their grandchildren ####
+
+# Go one step further by inner joining themes to a filtered version of itself again to establish a connection between the last join's children and their children (or parents' grandchildren)
+themes %>%
+  inner_join(themes, by = c("id" = "parent_id"), 
+             suffix = c("_parent", "_child")) %>%
+  inner_join(themes, by = c("id_child" = "parent_id"), 
+             suffix = c("_parent", "_grandchild"))
