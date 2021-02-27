10185fd2cdbc0868ae01cf4f86dbbec4454a61e0
Trang Ng <59824573+ltrangng@users.noreply.github.com>
2021-02-27 22:15:06 +0100

    readr

--- 1_code/4-1_Readr.R
+++ 1_code/4-1_Readr.R
@@ -1,7 +1,16 @@
-# 1 IMPORTING DATA
+# 1 READR
 
 # 1.1 Paths and the working directory ####
-# The first step when importing data from a spreadsheet is to locate the file containing the data.
+# The first step when importing data from a spreadsheet is to locate the file containing the data.  The simplest way to do this is to have a copy of the file in the folder in which the importing functions look by default.
+library(dslabs)
+# A spreadsheet containing the US murders data is included as part of the dslabs package.
+filename <- "murders.csv"
+dir <- system.file("extdata", package = "dslabs")
+fullpath <- file.path(dir, filename)
+file.copy(fullpath, "murders.csv")
+# This code does not read the data into R, it just copies a file. But once the file is copied, we can import the data with a simple line of code.
+
+#  The computer’s filesystem is a series of nested folders, each containing other folders and files.
 # directories:  folders that contain data.
 # root directory: folder that contain all other folders
 # working directory: the directory in which we are currently located. Get the full path of the current working directory without writing out explicitly:
@@ -12,17 +21,36 @@ wd
 dir()
 
 # The path of a file is a list of directory names that can be thought of as instructions on what folders to click on, and in what order, to find the file. 
+system.file(package = "dslabs")
 # full path: finding the file from the root directory.
 # relative path: finding the file starting in the working directory. It is recommended to use relative paths because full paths are unique to each computer and the code should be portable.
+dir <- system.file(package = "dslabs")
+list.files(path = dir)
+
+# Another example of obtaining a full path without writing out explicitly was given above when we created the object fullpath like this:
+filename <- "murders.csv"
+dir <- system.file("extdata", package = "dslabs")
+fullpath <- file.path(dir, filename)
+# The function system.file provides the full path of the folder containing all the files and directories relevant to the package specified by the package argument. By exploring the directories in dir we find that the extdata contains the file we want:
+dir <- system.file(package = "dslabs")
+filename %in% list.files(file.path(dir, "extdata"))
+# The system.file function permits us to provide a subdirectory as a first argument, so we can obtain the fullpath of the extdata directory like this:  
+dir <- system.file("extdata", package = "dslabs")
+# The function file.path is used to combine directory names to produce the full path of the file we want to import.
+fullpath <- file.path(dir, filename)
+
+# file.copy(): copies the file into home directory. This function takes two arguments: the file to copy and the name to give it in the new directory.
+file.copy(fullpath, "murders.csv")
+# Check if the file is in your working directory:
+list.files()
 
 # 1.2 readr ####
 # The readr library includes functions for reading data stored in text file spreadsheets into R. 
 library(readr)
 # We can open the file to examine or have a look at a few lines.
-read_lines("bakeoff.csv", n_max = 3)
-# When creating spreadsheets with text files, a new row is defined with return and columns are separated with some predefined special character. The most common characters are comma (,), semicolon (;), space ( ), and tab (a preset number of spaces or \t).
+read_lines("murders.csv", n_max = 3)
 # This also shows that there is a header. Now we are ready to read-in the data into R. From the .csv suffix and the peek at the file, we know to use read_csv:
-dat <- read_csv("bakeoff.csv")
+dat <- read_csv("murders.csv")
 # header: he first row contains column names rather than data. When we read-in data from a spreadsheet it is important to know if the file has a header or not. Most reading functions assume there is a header. 
 dat
 # The result is a tibble, not a just data frame, because read_csv is a tidyverse parser. We can confirm that the data has in fact been read-in with:
@@ -49,4 +77,3 @@ tmp_filename <- tempfile()
 download.file(url, tmp_filename)
 dat <- read_csv(tmp_filename)
 file.remove(tmp_filename) # This command erases the temporary file once it imports the data.
-  
